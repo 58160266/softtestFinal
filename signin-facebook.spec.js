949f72c0e2eebf6fcc8dace5e2ed8a01f4fb88d7
58160266 <58160266@go.buu.ac.th>
2017-11-30 23:38:35 +0700

    var 1.5

--- signin-facebook.spec.js
+++ signin-facebook.spec.js
@@ -29,3 +29,40 @@ test('Simple Mock',() => {
   expect(result).toBe('Hello panumat!')
   //expect(result).toEqual('Hello panumat!')
 })
+
+
+function BuuAuthen(service){
+  this.service = service
+
+  this.signIn = (username,password) =>{
+    var obj = service(username,password)
+    return {
+          name : obj.nickname,
+          token : '000889'
+          }
+  }
+}
+
+
+test('Sign-in with Facebook',()=>{
+  const object = {
+    nickname : "yo" ,
+    phone : 12345667,
+    email : "zaza@go.buu.ac.th"
+  }
+  const mockBuuAuth = jest.fn("Panumat","123456")
+      .mockReturnValue(object)
+  var auth = new BuuAuthen(mockBuuAuth)
+
+  var username = "Panumat"
+  var password = "123456"
+  var accouthInfo = auth.signIn(username,password)
+
+  expect(accouthInfo.name).toBe("yo")
+  expect(accouthInfo.token).toBe('000889')
+  expect(mockBuuAuth).toHaveBeenCalledWith(username,password)
+  expect(mockBuuAuth).toHaveBeenCalled()
+  expect(accouthInfo).toHaveProperty('token')
+  expect(accouthInfo.token).toHaveLength(6)
+
+})
